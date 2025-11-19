889c83c6ea21196a937f07bb2a0d6713deed8eb7
atharva0-7 <atharva.bhagwat@coditas.com>
2025-11-19 11:28:11 +0530

    simple demo of liquid glss renderer

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:liquid_glass_demo/dashboard_page.dart';
+
+
+void main() {
+  runApp(const MyApp());
+}
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Liquid Glass Demo',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const DashboardPage(),
+    );
+  }
+}
